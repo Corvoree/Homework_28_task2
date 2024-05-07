f8855b0ac6a95e3f137c0b2a92d5390bc67b3a49
Timur Vorontsov <voroncov.timur0@gmail.com>
2024-05-07 15:46:36 +0600

    Исправил во всех ??? на использование своих действий. Добавил ссылки на реализованные методы для вывода как в примере комментария. Добавил импорт.

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 package kg.attractor.java.task2;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,9 +13,14 @@ public class Main {
         
         // каждый кот должен уметь выполнять что-то своё
         var cats = new ArrayList<ActiveCat>();
-        cats.add(new ActiveCat(???));
-        cats.add(new ActiveCat(???));
-        cats.add(new ActiveCat(???));
+
+        cats.add(new ActiveCat(ActiveCat::jump));
+        cats.add(new ActiveCat(ActiveCat::sleep));
+        cats.add(new ActiveCat(ActiveCat::eat));
+
+        cats.add(new ActiveCat(() -> "Я играю с клубком"));
+        cats.add(new ActiveCat(() -> "Я смотрю на птиц!"));
+        cats.add(new ActiveCat(() ->"Я точу когти!"));
         // добавьте ещё два-три кота, с совершенно другими действиями
 
         cats.forEach(ActiveCat::doAction);
